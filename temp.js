7afa76e468915c23a2b00cbf15fb7317bba30900
HBZ <2961515523@qq.com>
2024-05-16 10:42:54 +0800

    ttemp

---
+++ temp.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         demo01
+// @namespace    http://tampermonkey.net/
+// @version      2024-05-16
+// @description  try to take over the world!
+// @author       HBZ
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log('你好，油猴');
+})();
