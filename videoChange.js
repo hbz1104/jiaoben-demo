0eac308c756dc820be3d93594c615379fa6d730e
hbz1104 <2961515523@qq.com>
2024-05-17 09:01:54 +0800

    videoChange

---
+++ videoChange.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         videoChange
+// @namespace    http://tampermonkey.net/
+// @version      v0.1
+// @description  简单视频倍速改变
+// @author       HBZ
+// @match        *://lms.ouchn.cn/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @license      AGPL-3.0
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let v1 = document.getElementsByTagName("video");
+    v1.playbackRate = 16;
+})();
